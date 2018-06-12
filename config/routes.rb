15af2210d9e9245b1978571720470259b800fc96
EikiKanaoka <kakakaeee@gmail.com>
2018-06-12 19:33:19 +0900

    db:migrate

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,5 @@ Rails.application.routes.draw do
   resources :posts
   resources :tests
   root to: 'top#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post '/callback' => 'linebots#callback'
 end
