36b158e18b2d731ca41e9785181f9a3962dcdd1a
EikiKanaoka <kakakaeee@gmail.com>
2018-06-12 19:30:23 +0900

    Destory @postの処理

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
   resources :hoges
   resources :posts
-  root to: 'top#index'
-
   resources :tests
-  root to: 'tests#index'
+  root to: 'top#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post '/callback' => 'linebots#callback'
 end
